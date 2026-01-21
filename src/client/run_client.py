4f1929677adfe3d8664563f6fe0edd701f037ca2
yuvank <88584152+yuvank@users.noreply.github.com>
2026-01-21 02:00:49 +0100

    Added ability to choose which server to connect while runnig client

--- src/client/run_client.py
+++ src/client/run_client.py
@@ -1,11 +1,20 @@
+# src/client/run_client.py
+import sys
 from src.client.chat_client import ChatClient
 
 if __name__ == "__main__":
-    client = ChatClient(username="alice")
-    client.start(ip="127.0.0.1", port=5001)
+    if len(sys.argv) != 3:
+        print("Usage: python -m src.client.run_client <ip> <port>")
+        sys.exit(1)
 
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+
+    client = ChatClient(username="user")
+    client.start(ip=ip, port=port)
     client.join_room("room1")
 
     while True:
         msg = input("> ")
         client.send_message(msg, "room1")
+
