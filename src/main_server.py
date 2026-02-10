b9803dbfff548eac224bc8a20c9bee7a1643ac09
yuvank <88584152+yuvank@users.noreply.github.com>
2026-02-10 12:52:04 +0100

    Removed temporary testing for room selection.

--- src/main_server.py
+++ src/main_server.py
@@ -1,7 +1,6 @@
 import sys
 import os
 from src.server.server_node import ServerNode
-import uuid
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -13,7 +12,7 @@ if __name__ == "__main__":
     rooms = int(sys.argv[2]) if len(sys.argv) > 2 else 1 #Default 1 room
 
     server = ServerNode(
-        server_id=os.getpid(), # It was os.getpid() uuid.uuid4()
+        server_id=os.getpid(), # It was os.getpid() What about string?
         ip_address="0.0.0.0",
         port=port,
         number_of_rooms=rooms,
